a8dfe6702dbe9d16ad64ae2c16ea0192f80545bc
kiyouk <kiyouapp@gmail.com>
2017-01-19 16:49:48 +0900

    Test 추가

---
+++ HelloiPhone/HelloiPhone/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloiPhone
+//
+//  Created by cscoi028 on 2017. 1. 19..
+//  Copyright © 2017년 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
